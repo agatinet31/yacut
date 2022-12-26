48983d32a3f18ac21c0214783aeeb04c134e919e
andrew <agatinet@gmail.com>
2022-12-26 11:41:20 +0300

    HW21

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import os
 
 class Config(object):
     """Класс конфигурации приложения."""
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'SECRET_KEY')
     LENGTH_SHORT_ID = 6
